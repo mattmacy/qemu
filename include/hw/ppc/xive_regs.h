97164ebd833f2195db3fa78a93eba6d98df6617b
Cédric Le Goater <clg@kaod.org>
2018-09-25 08:22:28 +0200

    ppc/xive: introduce the XiveRouter model
    The XiveRouter models the second sub-engine of the overall XIVE
    architecture : the Interrupt Virtualization Routing Engine (IVRE).
    
    The IVRE handles event notifications of the IVSE through MMIO stores
    and performs the interrupt routing process. For this purpose, it uses
    a set of table stored in system memory, the first of which being the
    Interrupt Virtualization entries (IVE) table.
    
    The IVT associates an interrupt source number with an Event Queue
    Descriptor which will be used in a second phase of the routing process
    to identify a notification target.
    
    The XiveRouter is an abstract class which needs to be inherited from
    to define a storage for the IVT, and other upcoming tables. The
    'chip-id' atttribute is not strictly necessary for the sPAPR and
    PowerNV machines but it's a good way to test the routing algorithm.
    Without this atttribute, the XiveRouter could be a simple QOM
    interface.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

---
+++ include/hw/ppc/xive_regs.h
@@ -0,0 +1,30 @@
+/*
+ * QEMU PowerPC XIVE interrupt controller model
+ *
+ * Copyright (c) 2016-2018, IBM Corporation.
+ *
+ * This code is licensed under the GPL version 2 or later. See the
+ * COPYING file in the top-level directory.
+ */
+
+#ifndef _PPC_XIVE_REGS_H
+#define _PPC_XIVE_REGS_H
+
+/* IVE (Interrupt Virtualization Entry)
+ *
+ * One per interrupt source. Targets that interrupt to a given EQ
+ * and provides the corresponding logical interrupt number (EQ data)
+ */
+typedef struct XiveIVE {
+        /* Use a single 64-bit definition to make it easier to
+         * perform atomic updates
+         */
+        uint64_t        w;
+#define IVE_VALID       PPC_BIT(0)
+#define IVE_EQ_BLOCK    PPC_BITMASK(4, 7)        /* Destination EQ block# */
+#define IVE_EQ_INDEX    PPC_BITMASK(8, 31)       /* Destination EQ index */
+#define IVE_MASKED      PPC_BIT(32)              /* Masked */
+#define IVE_EQ_DATA     PPC_BITMASK(33, 63)      /* Data written to the EQ */
+} XiveIVE;
+
+#endif /* _PPC_XIVE_REGS_H */
